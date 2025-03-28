f6612359b608247114b50e5d9a8ba9fd9c28b9cb
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-03-28 07:02:43 +0530

    datashow complete

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,29 +1,42 @@
 import User from "../models/user.model.js"
 import { asyncHandler } from "../utilities/asyncHandler.utility.js";
-import ErrorHandler from "../utilities/errorHandler.utility.js";
+import errorHandler from "../utilities/errorHandler.utility.js";
 
 
-export const register = asyncHandler(async(req,res,next)=>{
-    
-        const{fullName,username,password,gender,} = req.body;
-        if (!fullName || !username || !password || !gender) {  
-            return next(new errorHandler("all  field are required ",400))
-              
-        }
-        const user = await User.findOne({username});
-        if(user){
-            return next(new errorHandler("user already exist ",400))
-        }
-        const newUser = await User.create({
-            username,
-            fullName,
-            password,
-            gender,
-        })
-        
-        res.send("hello register")
+export const register = asyncHandler(async (req, res, next) => {
+
+    const { fullName, username, password, email, gender, } = req.body;
+    if (!fullName || !username || !password || !email || !gender) {
+        return next(new errorHandler("all  field are required ", 400))
+
+    }
+    const user = await User.findOne({ username });
+    if (user) {
+        return next(new errorHandler("user already exist ", 400))
+    }
+
+    const avatarType = gender === "male" ? "boy" : "girl";
+    const avatar = `https://avatar.iran.liara.run/public/${avatarType}?username=${username}`;
+
+    const newUser = await User.create({
+        fullName,
+        username,
+        password,
+        email,
+        gender,
+        avatar
     })
-export const login = (req,res,next)=>{
+    res.status(200).json({
+        success: true,
+        responseData: {
+            newUser
+        }
+    });
+
+
+    res.send("hello register")
+})
+export const login = (req, res, next) => {
     res.send("i m login")
 }
 
