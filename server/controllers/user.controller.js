1cd45a446cbad1c6e68a133bf456dfeae9835e8e
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-03-28 22:35:08 +0530

    password hashed

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import { asyncHandler } from "../utilities/asyncHandler.utility.js";
 import errorHandler from "../utilities/errorHandler.utility.js";
+import bcrypt from "bcryptjs";
 
 
 export const register = asyncHandler(async (req, res, next) => {
@@ -15,13 +16,15 @@ export const register = asyncHandler(async (req, res, next) => {
         return next(new errorHandler("user already exist ", 400))
     }
 
+    const hashedPassword = await bcrypt.hash(password,10)
+
     const avatarType = gender === "male" ? "boy" : "girl";
     const avatar = `https://avatar.iran.liara.run/public/${avatarType}?username=${username}`;
 
     const newUser = await User.create({
         fullName,
         username,
-        password,
+        password:hashedPassword,
         email,
         gender,
         avatar
@@ -36,8 +39,34 @@ export const register = asyncHandler(async (req, res, next) => {
 
     res.send("hello register")
 })
-export const login = (req, res, next) => {
-    res.send("i m login")
-}
+
+export const login = asyncHandler(async (req, res, next) => {
+
+    const {  username, password } = req.body;
+    if ( !username || !password ) {
+        return next(new errorHandler("Please Enter a valid username and password ", 400))
+
+    }
+    const user = await User.findOne({ username });
+    if (!user) {
+        return next(new errorHandler("Please enter a valid username ", 400))
+    }
+
+    const isValidPassword = await bcrypt.compare(password,user.password)
+    if(!isValidPassword){
+        return next(new errorHandler("Please enter a valid username ", 400))
+    }
+
+    res.status(200).json({
+        success: true,
+        responseData: {
+            user
+        }
+    });
+
+
+    res.send("hello register")
+})
+
 
 
