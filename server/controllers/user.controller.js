d6ac5a4df3a5683b1af2422e342f06fd8ebe5f80
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-03-28 23:13:44 +0530

    generate token

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js"
 import { asyncHandler } from "../utilities/asyncHandler.utility.js";
 import errorHandler from "../utilities/errorHandler.utility.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 
 export const register = asyncHandler(async (req, res, next) => {
@@ -16,7 +17,7 @@ export const register = asyncHandler(async (req, res, next) => {
         return next(new errorHandler("user already exist ", 400))
     }
 
-    const hashedPassword = await bcrypt.hash(password,10)
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     const avatarType = gender === "male" ? "boy" : "girl";
     const avatar = `https://avatar.iran.liara.run/public/${avatarType}?username=${username}`;
@@ -24,15 +25,32 @@ export const register = asyncHandler(async (req, res, next) => {
     const newUser = await User.create({
         fullName,
         username,
-        password:hashedPassword,
+        password: hashedPassword,
         email,
         gender,
         avatar
     })
-    res.status(200).json({
+    const tokenData = {
+        _id: newUser?._id
+    }
+
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES })
+
+    res
+    .status(200)
+    .cookie("token",{
+        expire: new Date(
+            Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+        ),
+        httpOnly:true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite:'None'
+    })
+    .json({
         success: true,
         responseData: {
-            newUser
+            newUser,
+            token
         }
     });
 
@@ -42,8 +60,8 @@ export const register = asyncHandler(async (req, res, next) => {
 
 export const login = asyncHandler(async (req, res, next) => {
 
-    const {  username, password } = req.body;
-    if ( !username || !password ) {
+    const { username, password } = req.body;
+    if (!username || !password) {
         return next(new errorHandler("Please Enter a valid username and password ", 400))
 
     }
@@ -52,20 +70,38 @@ export const login = asyncHandler(async (req, res, next) => {
         return next(new errorHandler("Please enter a valid username ", 400))
     }
 
-    const isValidPassword = await bcrypt.compare(password,user.password)
-    if(!isValidPassword){
+    const isValidPassword = await bcrypt.compare(password, user.password)
+    if (!isValidPassword) {
         return next(new errorHandler("Please enter a valid username ", 400))
     }
 
-    res.status(200).json({
+    const tokenData = {
+        _id: user?._id
+    }
+
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES })
+
+
+    res
+    .status(200)
+    .cookie("token",{
+        expire: new Date(
+            Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+        ),
+        httpOnly:true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite:'None'
+    })
+    .json({
         success: true,
         responseData: {
-            user
+            user,
+            token
         }
     });
 
 
-    res.send("hello register")
+    // res.send("hello register")
 })
 
 
