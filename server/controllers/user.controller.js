9bf0afac294c9caeae7ab3115b36c0658bc4e518
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-04-26 23:53:12 +0530

    updatw

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -136,10 +136,10 @@ export const logout = asyncHandler(async (req, res, next) => {
 export const getOtherUsers = asyncHandler(async (req, res, next) => {
     const otherUsers = await User.find({ _id: { $ne: req.user._id } });
 
-    res.status(200).json({
-        success: true,
-        responseData: otherUsers,
-    });
+    // res.status(200).json({
+    //     success: true,
+    //     responseData: otherUsers,
+    // });
 });
 
 
